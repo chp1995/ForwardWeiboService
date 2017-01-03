13027bcb2e81f6377c61fdbaf5d11ce4811c244c
chp1995 <405593951@qq.com>
2017-01-04 01:37:19 +0800

    1

--- service/serviceImpl/ForwardWeiboServiceImpl.java
+++ service/serviceImpl/ForwardWeiboServiceImpl.java
@@ -5,12 +5,19 @@
 
 import java.util.List;
 
-class Forward implements ForwardWeiboService{
+import cn.edu.bjtu.weibo.dao.WeiboDAO;
+import cn.edu.bjtu.weibo.dao.UserDAO;
+import cn.edu.bjtu.weibo.model.Weibo;
+
+
+class ForwardWeiboServiceImpl implements ForwardWeiboService{
 
 	@Override
 	public int getForwardNumber(String weiboId) {
 		int number = 0;
-		// TODO 自动生成的方法存根
+		WeiboDAO weiboDAO=new WeiboDAO();
+		number = weiboDAO.getForwardNumber(weiboId);
+		// TODO 
 		return number;
 	}
 
@@ -18,14 +25,28 @@ class Forward implements ForwardWeiboService{
 	public List<User> getForwardUserIdList(String weiboId, int pageIndex,
 			int numberPerPage) {
 		List<User> user = null;
-		// TODO 自动生成的方法存根
+		WeiboDAO weiboDAO=new WeiboDAO();
+		user = weiboDAO.getForwardList( weiboId , pageIndex , numberPerPage );
+		
+		// TODO 
 		return user;
 	}
 
 	@Override
 	public boolean forwardWeiboAction(String userId, String weiboId) {
-		// TODO 自动生成的方法存根
+		// TODO
+		
+		UserDAO userDAO = new UserDAO();//forwarder
+		WeiboDAO weiboDAO=new WeiboDAO();
+		boolean result1 = userDAO.insertForwardWeibo( userId, weiboId );
+		
+		string forwardWeiboId = userDAO.getLastWeiboId(userId);//new weibo id
+	
+		boolean result2 = weiboDAO.insertForwardList( weiboId, forwardWeiboId);
+		if(result1&&result2)
+			return true;
 		return false;
+
 	}
 	
 }
